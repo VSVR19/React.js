2f244c315648065c73049d4e71356195b0f83b1b
VSVR19 <venkatvs19@gmail.com>
2025-02-16 11:50:39 +0530

    feat: getting started with next.js and server side rendering

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,68 +1,41 @@
-// import { useReducer, useState } from 'react';
 import { useReducer } from 'react';
 
 // The reducer function gets access to the CURRENT STATE and an ACTION
 // Action is what is sent in the dispatch function
 // The REDUCER FUNCTION returns the new state
-const countReducer = (currentState, action) => {
-  // console.log(currentState, action);
+const reducer = (currentState, action) => {
+  console.log(currentState, action);
 
-  if (action.type === 'increment') return currentState + 1;
-  else if (action.type === 'decrement') return currentState - 1;
-  else if (action.type === 'setCount') return action.payload;
-  // else return currentState;
+  return { count: 0, step: 1 };
 };
 
-const stepReducer = (currentState, action) => {};
-
 function DateCounter() {
-  // const [count, setCount] = useState(0);
+  const initialState = { count: 0, step: 1 };
 
-  // useReducer
-  // Takes in a REDUCER FUNCTION and an INITIAL STATE
+  // useReducer takes in a REDUCER FUNCTION and an INITIAL STATE
   // Returns the CURRENT STATE and a DISPATCH FUNCTION
-  // It returns the CURRENT STATE- just like useState()
-  // const [count, dispatch] = useReducer(countReducer, 0);
-  const [count, dispatchCount] = useReducer(countReducer, 0);
-
-  // const [step, setStep] = useState(1);
-  const [step, dispatchStep] = useReducer(stepReducer, 1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date('june 21 2027');
   date.setDate(date.getDate() + count);
 
   const onDecrementClick = function () {
-    // dispatch(-1);
-    // DISPATCH AN ACTION!
-    // dispatch({ type: 'decrement', payload: -1 });
-    dispatchCount({ type: 'decrement' });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
+    dispatch({ type: 'decrement' });
   };
 
   const onIncrementClick = function () {
-    // dispatch(1);
-    // dispatch({ type: 'increment', payload: 1 });
-    dispatchCount({ type: 'increment' });
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
+    dispatch({ type: 'increment' });
   };
 
   const defineCount = function (e) {
-    // setCount(Number(e.target.value));
-    dispatchCount({ type: 'setCount', payload: Number(e.target.value) });
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
   };
 
-  const defineStep = function (e) {
-    // setStep(Number(e.target.value));
-    dispatchStep({ type: 'setStep', payload: Number(e.target.value) });
-  };
+  const defineStep = function (e) {};
 
-  const onResetClick = function () {
-    // setCount(0);
-    // setStep(1);
-  };
+  const onResetClick = function () {};
 
   return (
     <div className="counter">
